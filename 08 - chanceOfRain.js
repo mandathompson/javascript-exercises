f615d19b1dda82c24adb13c4d5aa94f91a39086a
Amanda Thompson <mandathompson@yahoo.com>
2022-10-27 13:57:33 -0400

    Update 08 - chanceOfRain.js

--- 08 - chanceOfRain.js
+++ 08 - chanceOfRain.js
@@ -1,4 +1,5 @@
 // create a function chanceOfRain, that will return the probability of rain in percentage, based on a random number received from Math.random() method.
+// return Math.floor(Math.random() * max) with max as param
 
 const chanceOfRain =() => {
    
